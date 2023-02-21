9cddcebdf60ef4bfa8e5d40ee2745264196386b2
fenix302 <fenix302@naver.com>
2023-02-21 17:34:33 +0900

    버튼 수정

--- src/views/ViewButton.js
+++ src/views/ViewButton.js
@@ -1,30 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Button } from 'react-bootstrap';
 
 const ViewButton = () => {
   return (
     <>
-      <Link to="/Insert">
-        <button>게시글 등록</button>
-      </Link>
-      <Link to="/View">
-        <button>최근 게시글 보기</button>
-      </Link>
-      <Link to="/Update">
-        <button>최근 게시글 수정</button>
-      </Link>
-      <Link to="/Delete">
-        <button>최근 게시글 삭제</button>
-      </Link>
-      <Link to="/location">
-        <button>카카오 지도</button>
-      </Link>
-      <Link to="/about">
-        <button>회사 소개</button>
-      </Link>
-      <Link to="/Login">
-        <button>로그인</button>
-      </Link>
+        <Link to="/Insert">
+          <Button variant="outline-secondary" size='lg'>게시글 등록</Button>
+        </Link>
+        <Link to="/View">
+          <Button variant="outline-secondary" size='lg'>최근 게시글 보기</Button>
+        </Link>
+        <Link to="/Update">
+          <Button variant="outline-secondary" size='lg'>최근 게시글 수정</Button>
+        </Link>
+        <Link to="/Delete">
+          <Button variant="outline-secondary" size='lg'>최근 게시글 삭제</Button>
+        </Link>
+        <Link to="/location">
+          <Button variant="outline-secondary" size='lg'>카카오 지도</Button>
+        </Link>
+        <Link to="/about">
+          <Button variant="outline-secondary" size='lg'>회사 소개</Button>
+        </Link>
+        <Link to="/Login">
+          <Button variant="outline-secondary" size='lg'>로그인</Button>
+        </Link>
     </>
   );
 };
