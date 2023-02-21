f9fbd278ca61ea1276a8c567aa1e7d9f2d4e8cee
KiimMiinJii <mkim93810@gmail.com>
2023-02-21 17:56:18 +0900

    header

--- src/layout/header.js
+++ src/layout/header.js
@@ -3,20 +3,12 @@ import './header.css'
 const Header = () => {
     return(
         <header>
+            <nav className="navbar navbar-expand-lg navbar-light container justify-content-between" id="navbar-example2">
+                <ul class="navbar-brand">
+                    <li><a href="#"><img src="img/logo.png" alt="logo" /></a></li>
+                </ul>
 
-            <div className="contents">
-
-                <nav className="navbar navbar-expand-lg navbar-light container justify-content-between" id="navbar-example2">
-                    <ul class="navbar-brand">
-                        <li><a href="#"><img src="img/logo.png" alt="logo" /></a></li>
-                    </ul>
-
-                </nav>
-
-                <div className="first">                    
-                    <p className="h1">구로 문화센터</p>
-                </div>
-            </div>
+            </nav>
         </header>
     )
 }
