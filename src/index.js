b06b1159e57a73af092ec94e2812618dee93184a
KiimMiinJii <mkim93810@gmail.com>
2023-02-21 17:21:12 +0900

    header/img

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom"; // 페이지 이동 처리
+import 'bootstrap/dist/css/bootstrap.css'; //부트스트랩추가
 
 ReactDOM.render(
   <BrowserRouter>
