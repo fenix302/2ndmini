a34341da4948ff4349fb3a9ae29bdcdb063cb738
KiimMiinJii <mkim93810@gmail.com>
2023-02-21 15:44:41 +0900

    Footer

--- src/layout/footer.js
+++ src/layout/footer.js
@@ -1,10 +1,20 @@
 import './footer.css'
 
+
 const Footer = () => {
     return(
-        <footer className="footer">
+        <footer>
             <div className="contents">
-                <h2 className="title">이것은 푸터</h2>
+                <div className="first">
+                    <p className="h1">구로 문화센터</p>
+                    <p className="h4">
+                    대표자 | 홍길동 <br/>
+                    전화 | 02-488-4888 <br/>
+                    주소 | 04202 서울특별시 마포구 마포대로24길 16 (아현동) 114-401 <br/> 
+                    사업자등록번호 | 197-88-88888  <br/>
+                    통신판매업신고 | 제2021-서울마포-1933호 <br/>
+                    </p>
+                </div>
             </div>
         </footer>
     )
