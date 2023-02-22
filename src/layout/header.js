65e8d47b2185d2b8fc1b744b9aaaf0277d2ee6d4
KiimMiinJii <mkim93810@gmail.com>
2023-02-22 16:46:08 +0900

    header+bootstrap

--- src/layout/header.js
+++ src/layout/header.js
@@ -1,4 +1,5 @@
 import './header.css'
+import 'bootstrap/dist/css/bootstrap.css'
 
 const Header = () => {
     return(
@@ -7,6 +8,25 @@ const Header = () => {
                 <ul class="navbar-brand">
                     <li><a href="#"><img src="img/logo.png" alt="logo" /></a></li>
                 </ul>
+
+                {/* <!-- 네비게이션 바 --> */}
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>               
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">About</a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="${context}/company/introduce.jsp">회사소개</a></li>
+                                <li><a class="dropdown-item" href="${context}/company/location.jsp">오시는 길</a></li>
+                                <li><a class="dropdown-item" href="${context}/faq/faq.jsp">FAQ</a></li>
+                            </ul>
+                        </li>
+                        
+                    </ul>
+                </div>
 
             </nav>
         </header>
