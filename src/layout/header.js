4ce4c809b6b514c53df67bcde49c78ea27812f83
KiimMiinJii <mkim93810@gmail.com>
2023-02-22 18:42:28 +0900

    회사소개 연결

--- src/layout/header.js
+++ src/layout/header.js
@@ -1,4 +1,5 @@
 import './header.css'
+import about from "../test/about";
 import 'bootstrap/dist/css/bootstrap.css'
 import "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
 
@@ -21,9 +22,19 @@ const Header = () => {
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">About</a>
                             <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="${context}/company/introduce.jsp">회사소개</a></li>
-                                <li><a class="dropdown-item" href="${context}/company/location.jsp">오시는 길</a></li>
-                                <li><a class="dropdown-item" href="${context}/faq/faq.jsp">FAQ</a></li>
+                                <li><a class="dropdown-item" href="/about">회사소개</a></li>
+                                <li><a class="dropdown-item" href="#">오시는 길</a></li>
+                                <li><a class="dropdown-item" href="#">FAQ</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Community</a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="${context}/work/notice/retrieveBoardList.do">공지사항</a></li>
+                                <li><a class="dropdown-item" href="${context}/work/generalboard/retrieveBoardList.do">자유게시판</a></li>
+                                <li><a class="dropdown-item" href="${context}/work/comm/retrieveBoardList.do">커뮤니티</a></li>
+                                <li><a class="dropdown-item" href="${context}/work/consulting/retrieveBoardList.do">상담신청게시판</a></li>
                             </ul>
                         </li>
                         
