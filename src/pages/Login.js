054a839a3fd2bc9d5c6f3489f9dfd9e111374f1c
fenix302 <fenix302@naver.com>
2023-02-24 17:11:41 +0900

    로그인페이지 진행중

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,74 +6,86 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import "../css/login.css";
+import axios from 'axios';
 
 const Login = () =>  {
+  const [inputId, setInputId] = useState("");
+  const [inputPw, setInputPw] = useState("");
+  
+    const handleInputId = (e) => {
+    setInputId(e.target.value);
+  };
 
-    const history = useHistory(); // 라우팅을 위한 히스토리 객체
-    const [formData, setFormData] = useState(null);
-    const handleInputChange = (e) => {
-      // 아이디와 비밀번호가 실시간으로 바뀔때마다, state에 값이 업데이트 됩니다.
-      setFormData({
-        ...formData,
-        [e.target.name]: e.target.value,
-      });
-    };
+  const handleInputPw = (e) => {
+    setInputPw(e.target.value);
+  };
 
-    const handleSubmit = () => {
-        // 로그인 버튼을 눌렀을때 동작합니다.
-        fetch("http://localhost:3000/login", {
-          headers: {
-            "Content-Type": "application/json", // 파일업로딩이 없으니 json 타입입니다.
-            Accept: "application/json",
-          },
-          method: "POST",
-          body: JSON.stringify({
-            formData, // formData에는 아이디와 비밀번호가 들어가 있습니다.
-          }),
+  var submitYN = false;
+  function fn_signIn(){
+
+		if(document.getElementById("memberId").value.length < 1){
+			alert("아이디를 입력하세요.");
+			document.getElementById("memberId").focus()
+			return;
+		}
+		
+		if(document.getElementById("memberPw").value.length < 1){
+			alert("비밀번호를 입력하세요.");
+			document.getElementById("memberPw").focus()
+			return;
+		}
+
+		if(!submitYN){
+			submitYN = true;
+		
+      console.log("click login");
+      console.log("ID : ", inputId);
+      console.log("PW : ", inputPw);
+
+			axios({
+				 url :"http://localhost:9008/loginAction.do"
+				,type:"post"
+				,data:{
+          id : inputId,
+          passwd : inputPw,
+        }
+      })
+        .then((res) => {
+          console.log(res.data);
+          if(res.data != "N"){
+            alert(res.data + "회원님, 환영합니다");
+            document.location.href="/"
+          }else{
+            alert("회원정보 가입을 해주세요");
+          }
+        document.location.href = "/login";
         })
-          .then((result) => result.json())
-          .then((data) => {
-            if (data.success === true) {
-              // 서버로부터 true가 나오면, 2페이지로 이동하고, 환영합니다 메세지를 띄웁니다.
-              history.push("/management");
-            }
-    
-            alert(data.msg);
-          });
-      };
+          .catch()
+        
+    }
+		
+	}
+
 
     
     return (
         <>
-            <style type="text/css">
-                {`
-                    .btn-flat {
-                        background-color: purple;
-                        color: white;
-                    }
-
-                    .btn-join{
-                        background-color: orange;
-                        color: white;
-                    }
-                `}
-            </style>
 
             <Container fluid="sm">
                 <Row>
                     <Col md={{ span: 4, offset: 4 }}>
                         <h1 className='login_txt'>로그인</h1>
-                        <Form>
-                            <Form.Group className="mb-3" controlId="formBasicId">
-                                <Form.Control type="id" placeholder="아이디를 입력하세요" />
+                        <Form id='loginForm'>
+                            <Form.Group className="mb-3">
+                                <Form.Control type="id" id="memberId" placeholder="아이디를 입력하세요" value={inputId} onChange={handleInputId}/>
                             </Form.Group>
 
-                            <Form.Group className="mb-3" controlId="formBasicPassword">
-                                <Form.Control type="password" placeholder="비밀번호를 입력하세요" />
+                            <Form.Group className="mb-3">
+                                <Form.Control type="password" id="memberPw" placeholder="비밀번호를 입력하세요" value={inputPw} onChange={handleInputPw}/>
                             </Form.Group>
                             
                             <div className="d-grid gap-2 mb-3">
-                                <Button variant="primary" size='lg' onClick={handleSubmit}><Link to={"/management"}>로그인</Link></Button>
+                                <Button variant="primary" size='lg' onClick={fn_signIn}>로그인</Button>
                                 <Button variant="secondary" size='lg'><Link to={"/register"}>회원가입</Link></Button>
                             </div>
                         </Form>
