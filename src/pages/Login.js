b84cbb033cb6adce927e7a75afce8314eed8cc0d
flyepic4 <flyepic4@gmail.com>
2023-02-27 17:55:50 +0900

    로그인

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,64 +1,64 @@
 import React, { Component, useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
-import { Button } from 'react-bootstrap';
+import { Button, FormControl } from 'react-bootstrap';
 import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import "../css/login.css";
 import axios from 'axios';
+import { Input } from 'reactstrap';
 
 const Login = () =>  {
-  const [inputId, setInputId] = useState("");
-  const [inputPw, setInputPw] = useState("");
-  
-    const handleInputId = (e) => {
-    setInputId(e.target.value);
-  };
 
-  const handleInputPw = (e) => {
-    setInputPw(e.target.value);
-  };
 
-  var submitYN = false;
-  function fn_signIn(){
+  const fn_signIn = () => {
+    var submitYN = false;
 
-		if(document.getElementById("memberId").value.length < 1){
-			alert("아이디를 입력하세요.");
+    const memberId = document.getElementById('memberId').value;
+    const memberPw = document.getElementById('memberPw').value;
+
+    
+    
+    if(document.getElementById("memberId").value.length < 1){
+      alert("아이디를 입력하세요.");
 			document.getElementById("memberId").focus()
 			return;
 		}
 		
 		if(document.getElementById("memberPw").value.length < 1){
-			alert("비밀번호를 입력하세요.");
+      alert("비밀번호를 입력하세요.");
 			document.getElementById("memberPw").focus()
 			return;
 		}
-
+    
 		if(!submitYN){
-			submitYN = true;
-		
-      console.log("click login");
-      console.log("ID : ", inputId);
-      console.log("PW : ", inputPw);
+      var submitYN = true;
+      
+      const form = new FormData()
+        form.append('memberId', memberId)
+        form.append('memberPw', memberPw)
+      
+        console.log("click login");
 
-			axios({
-				 url :"http://localhost:9008/loginAction.do"
-				,type:"post"
-				,data:{
-          id : inputId,
-          passwd : inputPw,
+			axios.post('http://localhost:9008/loginAction.do', 
+          form
+        ,{ 
+          headers:{ 
+           'Content-type': 'application/json', 
+             } 
         }
-      })
-        .then((res) => {
+        )
+      .then((res) => {
           console.log(res.data);
+
           if(res.data != "N"){
             alert(res.data + "회원님, 환영합니다");
-            document.location.href="/"
+            document.location.href="/management"
           }else{
             alert("회원정보 가입을 해주세요");
+            document.location.href = "/login";
           }
-        document.location.href = "/login";
         })
           .catch()
         
@@ -66,22 +66,19 @@ const Login = () =>  {
 		
 	}
 
-
-    
     return (
         <>
-
             <Container fluid="sm">
                 <Row>
                     <Col md={{ span: 4, offset: 4 }}>
                         <h1 className='login_txt'>로그인</h1>
                         <Form id='loginForm'>
                             <Form.Group className="mb-3">
-                                <Form.Control type="id" id="memberId" placeholder="아이디를 입력하세요" value={inputId} onChange={handleInputId}/>
+                                <Input type="id" id="memberId" placeholder="아이디를 입력하세요" />
                             </Form.Group>
 
                             <Form.Group className="mb-3">
-                                <Form.Control type="password" id="memberPw" placeholder="비밀번호를 입력하세요" value={inputPw} onChange={handleInputPw}/>
+                                <Input type="password" id="memberPw" placeholder="비밀번호를 입력하세요" />
                             </Form.Group>
                             
                             <div className="d-grid gap-2 mb-3">
