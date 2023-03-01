5c4c802de589e18c7a3334055f2bc8521eeba28d
flyepic4 <flyepic4@gmail.com>
2023-03-01 20:09:10 +0900

    푸터아이콘

--- src/layout/footer.js
+++ src/layout/footer.js
@@ -1,21 +1,29 @@
 import './footer.css'
+import Container from 'react-bootstrap/Container';
 
 
 const Footer = () => {
     return(
         <footer>
-            <div className="contents">
-                <div className="first">
-                    <p className="h1">구로 문화센터</p>
-                    <p className="h4">
-                    대표자 | 홍길동 <br/>
-                    전화 | 02-488-4888 <br/>
-                    주소 | 04202 서울특별시 구로구 구로중앙로 152 <br/> 
-                    사업자등록번호 | 197-88-88888  <br/>
-                    통신판매업신고 | 제2021-서울구로-1933호
-                    </p>
+            <Container fluid="sm">
+                <div className="contents">
+                    <div className="first">
+                        <p className="h1">구로 문화센터</p>
+                        <p className="h4">
+                        대표자 | 홍길동 <br/>
+                        전화 | 02-488-4888 <br/>
+                        주소 | 04202 서울특별시 구로구 구로중앙로 152 <br/> 
+                        사업자등록번호 | 197-88-88888  <br/>
+                        통신판매업신고 | 제2021-서울구로-1933호
+                        </p>
+                    </div>
                 </div>
-            </div>
+                <div className='contents_img'>
+                    <a href='#'><img src='/img/sns-facebook.png'></img></a>
+                    <a href='#'><img src='/img/sns-instagram.png'></img></a>
+                    <a href='#'><img src='/img/sns_ytb2.png'></img></a>
+                </div>
+            </Container>
         </footer>
     )
 }
