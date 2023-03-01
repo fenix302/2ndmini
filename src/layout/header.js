aedfae4ddd5e0faab864912b1f68699e482c3aa0
flyepic4 <flyepic4@gmail.com>
2023-03-01 14:32:45 +0900

    공지, 커뮤니티 삭제

--- src/layout/header.js
+++ src/layout/header.js
@@ -87,38 +87,15 @@ const Header = () => {
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle main_nav" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Community</a>
                             <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="${context}/work/notice/retrieveBoardList.do">공지사항</a></li>
                                 <li>                            
                                     <Link to="/BoardList">
                                         <a class="dropdown-item">자유게시판</a>
                                     </Link>
                                 </li>
-                                <li><a class="dropdown-item" href="${context}/work/comm/retrieveBoardList.do">커뮤니티</a></li>
                                 <li><a class="dropdown-item" href="${context}/work/consulting/retrieveBoardList.do">상담신청게시판</a></li>
                             </ul>
                         </li>  
 
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle main_nav" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Other</a>
-                            <ul class="dropdown-menu">
-                                <li>
-                                    <Link to="/">
-                                        <a class="dropdown-item">무언가 생기겠죠?</a>
-                                    </Link>
-                                </li>
-                            </ul>
-                        </li>
-
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle main_nav" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Other</a>
-                            <ul class="dropdown-menu">
-                                <li>
-                                    <Link to="/">
-                                        <a class="dropdown-item">무언가 생기겠죠?</a>
-                                    </Link>
-                                </li>
-                            </ul>
-                        </li>                          
                     </ul>
 
                     
