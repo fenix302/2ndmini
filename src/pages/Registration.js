7fe66b1ba793cb0b575e7f61e9d500dc0f63d6f8
lucelin0414 <lucelin0414@gmail.com>
2023-03-01 21:26:44 +0900

    재수정

--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React from "react";
+import React,{useEffect} from "react";
 import { Container, Row, Col, Table, Button } from "reactstrap";
 import '../css/registration.css'
 
