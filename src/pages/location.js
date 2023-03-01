a3ac98c642a305b60f2f3c173f8fb62736f668c0
flyepic4 <flyepic4@gmail.com>
2023-03-01 21:07:27 +0900

    오시는길 주소변경

--- src/pages/location.js
+++ src/pages/location.js
@@ -9,7 +9,7 @@ const Kakaomap = () => {
     useEffect(()=> {
       const mapContainer = document.getElementById('map'); // 지도를 표시할 div 
       const mapOption = { 
-               center: new kakao.maps.LatLng(37.567283765403744, 126.97895774407993), // 지도의 중심좌표
+               center: new kakao.maps.LatLng(37.503174, 126.879052), // 지도의 중심좌표
                level: 3 // 지도의 확대 레벨
       };
 
@@ -52,7 +52,7 @@ const Kakaomap = () => {
           <div className='text1'>
             <span className='name2'>&nbsp;문화재단&nbsp;</span>
             <p className="announce">
-               <strong> ▷ 서울특별시 중구 무교로 15 남강타워빌딩 2층</strong><br />
+               <strong> ▷ 서울특별시 구로구 구로2동 4, 202호</strong><br />
                   방문 시에는 반드시 마스크를 착용해 주시고, 코로나19 방역수칙을 지켜주시기
                   바랍니다.
                </p>
@@ -70,7 +70,7 @@ const Kakaomap = () => {
                     주소
                   </th>
                   <td>
-                    서울특별시 중구 무교로 15 남강타워빌딩 2층
+                  서울특별시 구로구 구로2동 4, 202호
                   </td>
                 </tr>
                 <tr>
@@ -78,7 +78,7 @@ const Kakaomap = () => {
                     전화
                   </th>
                   <td>
-                    02-2096-0160
+                   02-2615-9321
                   </td>
                 </tr>
                 <tr>
@@ -94,7 +94,7 @@ const Kakaomap = () => {
                     지하철
                   </th>
                   <td>
-                  - 지하철 1호선 시청역 4번 출구 방향 <br />
+                  - 지하철 1호선 구로역 2번 출구 방향 <br />
                   </td>
                 </tr>
               </tbody>
@@ -104,22 +104,21 @@ const Kakaomap = () => {
           <br />
 
           <div className='text2'>
-            <span class="name2"><br /> <br />&nbsp;서울시청 주차장 안내&nbsp;</span>
+            <span class="name2"><br /> <br />&nbsp;구로문화센터 주차장 안내&nbsp;</span>
               <p class="announce2">
                   ▷ 주차공간이 많이 부족하오니 되도록 대중교통을 이용해 주시기 바랍니다.<br />
                   ▷ 주차정보안내시스템(<a href="http://parking.seoul.go.kr/"
-                     >http://parking.seoul.go.kr/</a>)에서 시청 주변 공영주차장을 확인하실 수 있습니다. <br />
-                  ▷ 청사 시설물을 보호하기 위해 높이 2.3m 이상 차량은 주차장으로 진입할 수 없습니다.<br /><br />
+                     >http://parking.seoul.go.kr/</a>)에서 주변 공영주차장을 확인하실 수 있습니다. <br />
+                  ▷ 시설물을 보호하기 위해 높이 2.3m 이상 차량은 주차장으로 진입할 수 없습니다.<br /><br />
               </p>
             <span class="name2">&nbsp;개방시간&nbsp;</span>
               <p class="announce2">
                 <strong> 평일</strong> : 08:30~21:00 <br />
-                <strong> 주말 및 공휴일</strong> : 09:00 ~21:00 (시민청 개방시간과 동일)<br />
+                <strong> 주말 및 공휴일</strong> : 09:00 ~21:00 <br />
                 <thin>
                     매월 넷째 주 수요일 '대중교통 이용의 날'은 서울시 및 산하기관, 자치구 부설
                     주차장이 폐쇄됩니다.(장애인 차량, 긴급차량 등 제외)<br />
-                    서울광장 행사 등으로 인해 주차수요 과포화 및 교통 혼잡이 예상될 경우 주차장
-                    이용이 제한될 수 있습니다.</thin><br />
+                    주차수요 과포화 및 교통 혼잡이 예상될 경우 주차장 이용이 제한될 수 있습니다.</thin><br />
                 <br />
               </p>
             <span class="name2">&nbsp;주차요금&nbsp;</span>
