074c73ccdc349ab566f9bee323f6adf4b7ca61dc
flyepic4 <flyepic4@gmail.com>
2023-03-01 20:56:35 +0900

    변경사항

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import { Route, Switch } from "react-router-dom";
 import InputForm from "./views/InputForm";
-import ViewButton from "./views/ViewButton";
+import Main from "./views/Main";
 import Header from "./layout/header";
 import Footer from "./layout/footer";
 import about from "./test/about";
 import Kakaomap from "./pages/location";
 import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Home from "./pages/Home";
 import LectureList from "./pages/LectureList";
 import BoardList from "./pages/BoardList";
 import Faq from "./pages/Faq";
@@ -27,13 +25,11 @@ function App(){
           <Route exact path="/LectureList" component={LectureList} />
           <Route exact path="/about" component={about} />
           <Route exact path="/Login" component={Login} />
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/management" component={Home} />
           <Route exact path="/registration" component={Registration} />
           <Route exact path="/location" component={Kakaomap} />
           <Route exact path="/Faq" component={Faq} />
           <Route exact path="/:crud" component={InputForm} />
-          <Route exact path="/" component={ViewButton} />
+          <Route exact path="/" component={Main} />
           
         </Switch>
     
